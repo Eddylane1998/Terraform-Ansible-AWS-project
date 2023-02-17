c4f530fe7d210b3c3df3399dc0947d0efceafbbc
Eddylane1998 <dylan.vd.kleijn@gmail.com>
2023-02-17 17:12:10 +0100

    Adding nginx install

--- main.tf
+++ main.tf
@@ -9,6 +9,12 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+locals {
+  vpc_id    = "vpc-0ff2bf3f6a6b0abe6"
+  subnet_id = "subnet-076ed149f5c3edf12"
+  ssh_user  = "Ubuntu"
+}
+
 provider "aws" {
   region  = "eu-west-2"
 }
@@ -29,11 +35,58 @@ data "aws_ami" "amazon-linux-2" {
  }
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_security_group" "nginx" {
+  name   = "nginx_access"
+  vpc_id = local.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx" {
   ami = "${data.aws_ami.amazon-linux-2.id}"
+  subnet_id = local.subnet_id
   instance_type = "t2.micro"
+  associate_public_ip_address = true
+  security_groups = [aws_security_group.nginx.id]
 
   tags = {
     Name = "Terraform_Github_Ansible"
   }
+}
+
+provisioner "remote-exec" {
+  inline = ["echo 'wait until ssh is ready'"]
+
+  connection {
+    type = "ssh"
+    user = local.ssh_user
+    host = aws_instance.nginx.public_ip
+  }
+}
+
+provisioner "local-exec" {
+  command = "ansible-playbook -i ${aws_instance.nginx.public_ip}"
+}
+
+output "nginx_ip" {
+  value = aws_instance.nginx.public_ip  
 }

f1f582d5736833640775662e353fc968d1d9533f
Eddylane1998 <dylan.vd.Kleijn@gmail.com>
2023-02-18 20:52:06 +0100

    adding SSH key and starting with EKS clusters

--- main.tf
+++ main.tf
@@ -1,12 +1,27 @@
 terraform {
-  required_providers {
+required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 4.47.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.2.0"
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = "~> 1.3"
 }
 
 locals {
